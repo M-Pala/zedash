97681ef702df5c03e3ded70475256eb3787581fa
Madhav Panchhiwala <madhav2panchhiwala@gmail.com>
2022-09-03 11:56:25 +0530

    upgrade version

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import { HashRouter as Router } from 'react-router-dom';
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//       <App />
+//     </Router>
+//   </React.StrictMode>
+// )
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
